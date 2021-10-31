b5bc1899df2c2ebb4041503966ab4f6fe099b6e6
Nobuyuki Nakata <nobu.nakata@gmail.com>
2021-10-31 23:16:02 +0900

    Rect で座標返還

---
+++ Runtime/Extensions/RectExtension.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist2.Extensions.RectExt {
+
+    public static class RectExtension {
+
+        public static Vector2 TransformPosition(this Rect rect, Vector2 pos)
+            => new Vector2(rect.width * pos.x + rect.x, rect.height * pos.y + rect.y);
+
+        public static Vector2 InverseTransformPosition(this Rect rect, Vector2 pos)
+            => new Vector2((pos.x - rect.x) / rect.width, (pos.y - rect.y) / rect.height);
+    }
+}
