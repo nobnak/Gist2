2cd3eefdbe9f51089980816523910f9e0fffbbf9
Nobuyuki Nakata <nobu.nakata@gmail.com>
2021-11-02 01:33:41 +0900

    Rect でベクトルの返還

--- Runtime/Extensions/RectExtension.cs
+++ Runtime/Extensions/RectExtension.cs
@@ -8,8 +8,14 @@ namespace Gist2.Extensions.RectExt {
 
         public static Vector2 TransformPosition(this Rect rect, Vector2 pos)
             => new Vector2(rect.width * pos.x + rect.x, rect.height * pos.y + rect.y);
+        
+        public static Vector2 TransformVector(this Rect rect, Vector2 pos)
+            => new Vector2(rect.width * pos.x, rect.height * pos.y);
 
         public static Vector2 InverseTransformPosition(this Rect rect, Vector2 pos)
             => new Vector2((pos.x - rect.x) / rect.width, (pos.y - rect.y) / rect.height);
+
+        public static Vector2 InverseTransformVector(this Rect rect, Vector2 pos)
+            => new Vector2(pos.x / rect.width, pos.y / rect.height);
     }
 }
