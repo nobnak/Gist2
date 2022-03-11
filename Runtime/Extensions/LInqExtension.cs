bcf20e4ae9c8a136480fc72e3f7402f48f6b8a55
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-03-11 22:46:50 +0900

    IEnumerator を IEnumerable に変換

--- Runtime/Extensions/LInqExtension.cs
+++ Runtime/Extensions/LInqExtension.cs
@@ -9,5 +9,9 @@ namespace Gist2.Extensions.LinqExt {
         public static void ForEach<T>(this IEnumerable<T> ts, System.Action<T> action) {
             foreach (var t in ts) action(t);
         }
+
+		public static IEnumerable<T> ToEnumerable<T>(this IEnumerator<T> iter) {
+			while (iter.MoveNext()) yield return iter.Current;
+		}
     }
 }
