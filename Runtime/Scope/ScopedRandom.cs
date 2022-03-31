97cdfb0df8cda12627454e76dd82c60440a70ce3
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-03-31 15:45:58 +0900

    + 乱数スコープ

---
+++ Runtime/Scope/ScopedRandom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist2.Scope {
+
+    public class ScopedRandom : System.IDisposable {
+
+        public readonly Random.State pastState;
+
+        public ScopedRandom(int seed) {
+            pastState = Random.state;
+            Random.InitState(seed);
+        }
+
+        public void Dispose() {
+            Random.state = pastState;
+        }
+    }
+}
