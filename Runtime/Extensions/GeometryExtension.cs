70cf7f470b96ef2915538e53b2791c5787fb6172
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-06-15 18:09:05 +0900

    線分からの距離を計算 別バージョン

--- Runtime/Extensions/GeometryExtension.cs
+++ Runtime/Extensions/GeometryExtension.cs
@@ -9,7 +9,7 @@ namespace Gist2.Extensions.GeometryExt {
 
 		public static readonly (float dist, float t) DEFAULT_DISTANCE_RESULT = (float.PositiveInfinity, -1);
 
-		public static (float dist, float t) Distance(this IEnumerable<Vector2> lineStrip, Vector2 v, Vector2 unit) {
+		public static (float dist, float t) DistanceFromLines(this IEnumerable<float2> lineStrip, float2 v, float2 unit) {
 			(float dist, float t) res = DEFAULT_DISTANCE_RESULT;
 
 			var iter = lineStrip.GetEnumerator();
@@ -23,16 +23,44 @@ namespace Gist2.Extensions.GeometryExt {
 				var seg1 = iter.Current * unit;
 
 				var lineseg = seg1 - seg0;
-				var len = lineseg.magnitude;
+				var len = math.length(lineseg);
 				var segtan = lineseg / len;
 				var wcos = Vector2.Dot(segtan, w - seg0);
 
 				var closestw = (wcos < 0 ? seg0 : (wcos > len ? seg1 : seg0 + wcos * segtan));
-				var dist = (w - closestw).magnitude;
+				var dist = math.length(w - closestw);
 				if (dist < res.dist) {
 					res = (dist, i + wcos / len);
 				}
 				
+				i++;
+				seg0 = seg1;
+			}
+
+			return res;
+		}
+		public static (float dist, float t) DistanceFromLines(this IEnumerable<float2> lineStrip, float2 v) {
+			(float dist, float t) res = DEFAULT_DISTANCE_RESULT;
+
+			var iter = lineStrip.GetEnumerator();
+			if (!iter.MoveNext()) return res;
+			var seg0 = iter.Current;
+
+			var i = 0;
+			while (iter.MoveNext()) {
+				var seg1 = iter.Current;
+
+				var lineseg = seg1 - seg0;
+				var len = math.length(lineseg);
+				var segtan = lineseg / len;
+				var vcos = Vector2.Dot(segtan, v - seg0);
+
+				var closestw = (vcos < 0 ? seg0 : (vcos > len ? seg1 : seg0 + vcos * segtan));
+				var dist = math.length((float2)(v - closestw));
+				if (dist < res.dist) {
+					res = (dist, i + vcos / len);
+				}
+
 				i++;
 				seg0 = seg1;
 			}
