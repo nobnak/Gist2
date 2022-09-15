8909b214e847337b1a72437d005a7db8ac971236
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-09-15 16:45:02 +0900

    - Extent 計算

--- Runtime/Collections/Primitives/Extensions/AABBExt.cs
+++ Runtime/Collections/Primitives/Extensions/AABBExt.cs
@@ -18,5 +18,16 @@ namespace Gist2.Collections.Primitives.Extensions {
         public static void Size(this in AABB2 aabb, out float2 size) => size = (aabb.Max - aabb.Min);
         [BurstCompile]
         public static void Size(this in AABB3 aabb, out float3 size) => size = (aabb.Max - aabb.Min);
+
+        [BurstCompile]
+        public static void Extent(this in AABB2 aabb, out float2 extent) {
+            aabb.Size(out var size);
+            extent = 0.5f * size;
+        }
+        [BurstCompile]
+        public static void Extent(this in AABB3 aabb, out float3 extent) {
+            aabb.Size(out var size);
+            extent = 0.5f * size;
+        }
     }
 }
