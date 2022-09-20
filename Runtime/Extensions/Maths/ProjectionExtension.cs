11ca734a5c1cfc45e23340316bd396d9f9d0f3be
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-09-21 03:26:37 +0900

    matrix 生成の簡素化

--- Runtime/Extensions/Maths/ProjectionExtension.cs
+++ Runtime/Extensions/Maths/ProjectionExtension.cs
@@ -10,10 +10,11 @@ namespace Gist2.Extensions.Maths {
 		public static bool MakeQuadVerticesOnPlane(this IEnumerable<float2> v, out float4 z, float z0 = 1f) {
 			var p = v.Take(4).ToArray();
 
-			var hmat = float4x4.identity;
-			hmat[0] = p[1].x; hmat[4] = -p[2].x; hmat[8] = p[3].x;
-			hmat[1] = p[1].y; hmat[5] = -p[2].y; hmat[9] = p[3].y;
-			hmat[2] = 1f; hmat[6] = -1f; hmat[10] = 1f;
+            var hmat = new float4x4(
+                p[1].x, -p[2].x, p[3].x, 0,
+                p[1].y, -p[2].y, p[3].y, 0,
+                1f, -1f, 1f, 0,
+                0, 0, 0, 1);
 			var hinv = math.inverse(hmat);
 
 			var det = math.determinant(hmat);
