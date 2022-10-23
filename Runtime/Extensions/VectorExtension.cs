74b747fed199a45d77b1604f5d4ad54da48a9215
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-10-23 14:47:05 +0900

    Vector -> float

--- Runtime/Extensions/VectorExtension.cs
+++ Runtime/Extensions/VectorExtension.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Gist2.Extensions.VectorExt {
 
     public static class VectorExtension {
 
-        public static Vector2 Div(this float a, Vector2 b) => new Vector2(a / b.x, a / b.y);
-        public static Vector2 Div(this float a, Vector2Int b) => new Vector2(a / b.x, a / b.y);
+        public static float2 Div(this float a, float2 b) => new float2(a / b.x, a / b.y);
+        public static float2 Div(this float a, int2 b) => new float2(a / b.x, a / b.y);
 
-        public static float Cross(this Vector2 a, Vector2 b) {
+        public static float Cross(this float2 a, float2 b) {
             return a.x * b.y - a.y * b.x;
         }
 
-		public static int Area(this Vector2Int v) => v.x * v.y;
+		public static int Area(this int2 v) => v.x * v.y;
     }
 }
