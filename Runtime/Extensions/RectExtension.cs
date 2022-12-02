304fdf49499bda765eb8333836b9ebde3c3f52b9
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-12-02 13:09:05 +0900

    make float4 as rect

--- Runtime/Extensions/RectExtension.cs
+++ Runtime/Extensions/RectExtension.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Gist2.Extensions.RectExt {
@@ -17,5 +18,11 @@ namespace Gist2.Extensions.RectExt {
 
         public static Vector2 InverseTransformVector(this Rect rect, Vector2 pos)
             => new Vector2(pos.x / rect.width, pos.y / rect.height);
+
+        public static float4 CreateAsRect(float2 pos, float2 size) => new float4(pos, size);
+        public static float2 SizeAsRect(this float4 r) => r.zw;
+        public static float2 CenterAsRect(this float4 r) => new float2(r.x + r.z * 0.5f, r.y + r.w * 0.5f);
+        public static float2 MinAsRect(this float4 r) => r.xy;
+        public static float2 MaxAsRect(this float4 r) => r.xy + r.zw;
     }
 }
